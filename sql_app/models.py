1f3c83b081227519caacc8c2e7f7a98ec16164c3
Diego Tapia <root.chile@gmail.com>
2022-11-25 13:07:58 +0100

    update models

--- sql_app/models.py
+++ sql_app/models.py
@@ -1,12 +1,15 @@
-from sqlalchemy import Column, ForeignKey, Integer, String, Float
+from sqlalchemy import Column, ForeignKey, Integer, String, Float, Boolean
 from sqlalchemy.orm import relationship
 from db import Base
 
 class Center(Base):
     __tablename__ = 'centers'
 
-    id = Column(Integer, primary_key=True)
+    center_id = Column(Integer, primary_key=True)
     type = Column(String, nullable=False)
+    city_code = Column(Integer, nullable=False)
+    region_code = Column(Integer, nullable=False)
+    op_area = Column(Float, nullable=False)
 
 
 class Order(Base):
@@ -15,5 +18,9 @@ class Order(Base):
     id = Column(Integer, primary_key=True)
     week = Column(Integer, nullable=False)
     center_id = Column(Integer, ForeignKey('centers.id'))
+    meal_id = Column(Integer, nullable=False)
+    checkout_price = Column(Float, nullable=False)
+    base_price = Column(Float, nullable=False)
+    email_for_promotion = Column(Boolean, nullable=False)
+    homepage_featured = Column(Boolean, nullable=False)
     num_orders = Column(Integer, nullable=False)
-
