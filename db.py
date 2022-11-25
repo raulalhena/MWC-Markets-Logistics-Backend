65328af3a8e7e57592f3eff104458cdd2bddd7fc
Diego Tapia <root.chile@gmail.com>
2022-11-25 14:19:51 +0100

    hardcoded database testing

--- db.py
+++ db.py
@@ -7,7 +7,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = os.environ['DATABASE_URL']
+SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:NkxlMh9DS8@postgres.cgguaffp7qy0.us-east-1.rds.amazonaws.com:5432/postgres'
 
 
 engine = create_engine(
