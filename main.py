6d7b62de6f4d17b6f309960c22fe781c197d4e39
Diego Tapia <root.chile@gmail.com>
2022-11-25 16:20:52 +0100

    add cors

--- main.py
+++ main.py
@@ -12,6 +12,19 @@ import os
 
 app = FastAPI()
 
+from fastapi.middleware.cors import CORSMiddleware
+
+
+origins = [
+    "http://localhost:4200",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/center")
 def get_all_items(name: Optional[str] = None,db: Session = Depends(get_db)):
